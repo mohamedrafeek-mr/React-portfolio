e9093fed39ecb99b6137b8ccfbc6176474fcbf0c
mohamedrafeek-mr <rafirafibai@gmail.com>
2026-02-13 17:49:34 +0530

    Fixed project import path error

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-import Project from './Components/Project/Project'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import Project from './Components/Project/project'
 
 const App = () => {
   return (
